c45f3d073ebe3f0c987a46b0cdd3a0a0c5e6bce6
mKaneko <doseichi.letterfrom@gmail.com>
2023-05-16 12:08:50 +0900

    TODO:paramでストアド出来るようにする

--- dbwrapper.php
+++ dbwrapper.php
@@ -3,51 +3,100 @@
 abstract class AbDBBase {
     abstract public function connect();
     abstract public function close();
-    abstract public function query($sql);
+    abstract public function query($sql, $params = []);
 }
 trait ReadIniSetting {
-    function read_ini(?string $section = null) {
+    function read_ini(?string $section = NULL) {
         $filepath = 'path/to/inifile.ini';
         $result = [];
         if(is_null($section)) {
             $result = parse_ini_file($filepath);
         } else {
-            $result = parse_ini_file($filepath, true);
+            $result = parse_ini_file($filepath, TRUE);
             $result = $result[$section];
         }
+        if(strtolower($result['host']) === 'localhost') {
+            $result['host'] = '127.0.0.1';
+        }
         return $result;
+    }
+}
+trait MapStmt {
+    /**
+     * SQL文からストアドキーを抜き出す（PDO記述）
+     * 
+     * @param string $sql 検証するSQL文
+     */
+    function preg_stmt($sql) {
+        $result = [];
+        $matches = preg_match_all("/:([-\w]+)/", $sql, $result);
+        if($matches !== FALSE || $matches > 0) {
+            $match = [];
+            $tmp = [];
+            foreach($tmp as $result) { $match[] = $tmp[1]; }
+            unset($$tmp);
+            return $match;
+        }
+        return FALSE;
+    }
+    /**
+     * パラメータがストアドキーに"すべて"存在するか
+     * 
+     * @param array $stmt_keys ストアドキー
+     * @param ?array $params パラメータ？
+     */
+    function isset_keys($stmt_keys, $params) {
+        $result = [];
+        $key = '';
+        foreach($key as $params) { $result[] = array_key_exists($key, $stmt_keys); }
+        unset($key);
+        return !in_array(FALSE, $result, TRUE);
     }
 }
 
 class WrapMySQL extends AbDBBase {
     use ReadIniSetting;
-    private $dbconn = null;
+    use MapStmt;
+    private $dbconn = NULL;
     private $ini = [];
-    function __construct(?string $section = null, bool $autoconnect = true) {
+    function __construct(?string $section = NULL, bool $autoconnect = TRUE) {
         $this->ini = read_ini($section);
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         if($autoconnect) {
             $this->connect();
         }
     }
     function connect() {
         if(!is_null($this->dbconn)) {
-            $this->dbconn = mysqli_connect($this->ini['host'], $this->ini['user'], $this->ini['pass'], $this->ini['dbname']);
-            if(mysqli_connect_errno()) {
-                die('MySQL NOT Connected. ' . mysqli_connect_error);
-            }
+            $this->dbconn = new mysqli($this->ini['host'], $this->ini['user'], $this->ini['password'], $this->ini['dbname']);
+            $this->dbconn->set_charset('utf8mb4');
         }
     }
     function close() {
         mysqli_close($this->dbconn);
     }
-    function query($sql) {}
+    function query($sql, $params = NULL) {
+        $has_params = !(is_null($params) || (is_array($params) && count($params) == 0));
+        try {
+            $stmt = preg_stmt($sql);
+            if($stmt === FALSE && !$has_params) {
+                // query
+            } else {
+                if(!isset_keys($stmt, $params)) { throw new Exception(); }
+                // stmt
+            }
+        } catch(Exception $err) {
+            //
+        }
+    }
 }
 
 class WrapPostgreSQL extends AbDBBase {
     use ReadIniSetting;
-    private $dbconn = null;
+    use MapStmt;
+    private $dbconn = NULL;
     private $ini = [];
-    function __construct(?string $section = null, bool $autoconnect = true) {
+    function __construct(?string $section = NULL, bool $autoconnect = TRUE) {
         $this->ini = read_ini($section);
         if($autoconnect) {
             $this->connect();
@@ -55,12 +104,24 @@ class WrapPostgreSQL extends AbDBBase {
     }
     function connect() {
         if(!is_null($this->dbconn)) {
-            $this->dbconn = pg_connect(sprintf("host=%s dbname=%s user=%s password=%s", $this->ini['host'], $this->ini['dbname'], $this->ini['user'], $this->ini['pass']))
+            $this->dbconn = pg_connect(sprintf("host=%s dbname=%s user=%s password=%s", $this->ini['host'], $this->ini['dbname'], $this->ini['user'], $this->ini['password']))
                 or die('PostgreSQL NOT Connected. ' . pg_last_error());
         }
     }
     function close() {
         pg_close($this->dbconn);
     }
-    function query($sql) {}
+    function query($sql, $params = NULL) {
+        $has_params = !(is_null($params) || (is_array($params) && count($params) == 0));
+        try {
+            $stmt = preg_stmt($sql);
+            if($stmt === FALSE && !$has_params) {
+                // query
+            } else {
+                // stmt
+            }
+        } catch(Exception $err) {
+            //
+        }
+    }
 }
